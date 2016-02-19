d32de693e8170a81774223fbd1711bc520ec5f4e
Kiri Wagstaff <kiri.l.wagstaff@jpl.nasa.gov>
2016-02-18 16:31:29 -0800

    Pre-annotate ChemCam targets.

--- src/pre_annotate.py
+++ src/pre_annotate.py
@@ -15,6 +15,9 @@ textdir = '../text/lpsc15-C'
 
 # Reference files
 elementfile = '../ref/elements.txt'
+chemcamfile = '../ref/chemcam-targets.txt'
+
+mypunc = re.sub('_', '', string.punctuation)
 
 # Files to analyze
 dirlist = [fn for fn in os.listdir(textdir) if
@@ -22,48 +25,49 @@ dirlist = [fn for fn in os.listdir(textdir) if
 
 # Process lines from input file, annotate anything that matches 'items',
 # and write out the annotations to outf with type 'name'.
-def pre_annotate(lines, items, name, outf):
+# Number targets starting from start_t.
+def pre_annotate(lines, items, name, outf, start_t):
     # Initialize counters
-    target_ind = 1
+    target_ind = start_t
     span_start = 0
     span_end = 0
 
-    # Iterate through documents; output to .ann file
-    with open(annfile, 'w') as outf:
-        # Iterate through words and look for a match in the elements list
-        for l in lines:
-            # Specifying ' ' explicitly means that all single spaces
-            # will cause a split.  This way we can update span_start
-            # correctly even if there are multiple spaces present.
-            words = l.split(' ')
-            for w in words:
-                # Remove any trailing \n etc.
-                w_strip = w.strip()
-                # Remove any punctuation
-                w_strip = re.sub('[%s]' % re.escape(string.punctuation), 
-                                 '', w_strip)
-                if w_strip in items:
-                    # This handles leading and trailing punctuation,
-                    # but not cases where there is internal punctuation
-                    span_start_strip = span_start + w.index(w_strip)
-                    #print str(w.index(w_strip))
-                    span_end    = span_start_strip + len(w_strip) 
-                    # Format: Tx\tTarget <span_start> <span_end>\t<word>
-                    outf.write('T' + str(target_ind) + '\t' +
-                               name + ' ' + str(span_start_strip) + 
-                               ' ' + str(span_end) + '\t' +
-                               w_strip + '\n')
-                    # Set up for the next target
-                    target_ind += 1
-                else:
-                    span_end    = span_start + len(w_strip) 
+    # Iterate through words and look for a match in the elements list
+    for l in lines:
+        # Specifying ' ' explicitly means that all single spaces
+        # will cause a split.  This way we can update span_start
+        # correctly even if there are multiple spaces present.
+        words = l.split(' ')
+        for w in words:
+            # Remove any trailing \n etc.
+            w_strip = w.strip()
+            # Remove any punctuation, except '_'
+            w_strip = re.sub('[%s]' % re.escape(mypunc), '', w_strip)
+            if w_strip in items:
+                # This handles leading and trailing punctuation,
+                # but not cases where there is internal punctuation
+                span_start_strip = span_start + w.index(w_strip)
+                #print str(w.index(w_strip))
+                span_end    = span_start_strip + len(w_strip) 
+                # Format: Tx\tTarget <span_start> <span_end>\t<word>
+                outf.write('T' + str(target_ind) + '\t' +
+                           name + ' ' + str(span_start_strip) + 
+                           ' ' + str(span_end) + '\t' +
+                           w_strip + '\n')
+                # Set up for the next target
+                target_ind += 1
+            else:
+                span_end    = span_start + len(w_strip) 
                     
-                #print '<%s>, span %d to %d' % (w, span_start, span_end)
-                # Either way, update span_start
-                span_end   = span_start + len(w)
-                span_start = span_end + 1 # Assumes followed by space 
-                # (or newline, for final word in line)
-            span_start -= 1 # Uncount the newline (double-count)
+            #print '<%s>, span %d to %d' % (w, span_start, span_end)
+            # Either way, update span_start
+            span_end   = span_start + len(w)
+            span_start = span_end + 1 # Assumes followed by space 
+            # (or newline, for final word in line)
+        span_start -= 1 # Uncount the newline (double-count)
+
+    # return the latest target_ind for ongoing use
+    return target_ind
 
 
 ##########################################################
@@ -71,15 +75,31 @@ def pre_annotate(lines, items, name, outf):
 print 'Annotating elements in %d files from %s.' % \
     (len(dirlist), textdir)
 
-# Read in the elements files
+# Read in the elements file
 with open(elementfile, 'r') as inf:
     lines = inf.readlines()
     elements = [l.strip() for l in lines]
     # Add lower-case versions of long element names
     elements += [e.lower() for e in elements if len(e) > 2]
     
-print elements
+# Read in the Chemcam targets file
+with open(chemcamfile, 'r') as inf:
+    lines = inf.readlines()
+    chemcam_targets = [l.strip() for l in lines]
+    # Add a version with _ converted to space
+    chemcam_targets_a = [re.sub('_', ' ', cc) for cc in chemcam_targets \
+                             if '_' in cc]
+    # Add a version with space converted to _
+    chemcam_targets_b = [re.sub(' ', '_', cc) for cc in chemcam_targets \
+                             if ' ' in cc]
+    chemcam_targets += chemcam_targets_a
+    chemcam_targets += chemcam_targets_b
 
+print [c for c in chemcam_targets if 'Square' in c]
+ 
+print 'Read in %d ChemCam target names.' % len(chemcam_targets)
+
+# Iterate through documents; output to .ann file
 for fn in dirlist:
     print fn
 
@@ -90,8 +110,10 @@ for fn in dirlist:
     annfile = textdir + '/' + fn[0:-4] + '.ann'
 
     # Create the annotations
+    start_t = 1
     with open(annfile, 'w') as outf:
-        pre_annotate(lines, elements, 'Element', outf)
+        start_t = pre_annotate(lines, elements,        'Element', outf, start_t)
+        start_t = pre_annotate(lines, chemcam_targets, 'Target',  outf, start_t)
 
 #    sys.exit(0)
     
