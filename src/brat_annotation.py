65c687af879dcae2380c746fc5fb0ba781eb5099
Kiri Wagstaff <kiri.l.wagstaff@jpl.nasa.gov>
2016-08-23 18:44:51 -0700

    Updated MTE DB schema to separate text anchors from canonical target and component names.  Much cleaner!
    More info on the schema here:
    
    https://github.jpl.nasa.gov/wkiri/MTE/wiki

--- src/brat_annotation.py
+++ src/brat_annotation.py
@@ -9,6 +9,13 @@
 import sys, os
 import dbutils
 
+# Map annotation labels to database table names and columns
+lookups = {'Target':   ('targets', 'target_name'),   
+           'Element':  ('components', 'component_name'),
+           'Mineral':  ('components', 'component_name'),
+           'Material': ('components', 'component_name'),
+           'Feature':  ('components', 'component_name')}
+
 
 class BratAnnotation:
     def __init__(self, annotation_line, doc_id, username):
@@ -19,25 +26,29 @@ class BratAnnotation:
         splitline = annotation_line.strip().split('\t')
         self.annotation_id = splitline[0]
 
-        if splitline[0][0] == 'T': # target
+        if splitline[0][0] == 'T': # anchors (for targets, components, events)
+            self.type    = 'anchor'
             self.label   = splitline[1].split()[0]
             args         = splitline[1].split()[1:]
             self.start   = args[0]
             self.end     = args[-1]
             self.name    = splitline[2]
         elif splitline[0][0] == 'E': # event
+            self.type    = 'event'
             args         = splitline[1].split() 
-            self.label   = args[0].split(':')[0]+'_event'
+            self.label   = args[0].split(':')[0]
             self.anchor  = args[0].split(':')[1]
             args         = [a.split(':') for a in args[1:]]
             self.targets = [v for (t,v) in args if t == 'Targ']
             self.cont    = [v for (t,v) in args if t == 'Cont']
         elif splitline[0][0] == 'R': # relation
+            self.type    = 'relation'
             label, arg1, arg2 = splitline[1].split() # assumes 2 args
             self.label   = label
             self.arg1    = arg1.split(':')[1]
             self.arg2    = arg2.split(':')[1]
         elif splitline[0][0] == 'A': # attribute
+            self.type    = 'attribute'
             label, arg, value = splitline[1].split()
             self.label   = label
             self.arg1    = arg
@@ -45,101 +56,111 @@ class BratAnnotation:
         else:
             print 'Unknown annotation type:', splitline[0]
 
-
     def insert(self, cursor):
 
         # Insert into the appropriate table depending on the annotation type
 
-        if (self.label == 'Target' or 
-            self.label == 'Contains' or
-            self.label == 'DoesNotContain' or
-            self.label == 'StratRel'): 
-            # If it's just 'Contains' or 'DoesNotContain' with no arguments,
-            # it is the anchor for an 'event', so add it
-            # to the targets table for later reference (to get excerpts).
-            dbutils.insert_into_table(
-                cursor=cursor,
-                table='targets',
-                columns=['target_id', 'target_name', 'span_start', 'span_end'],
-                values=[self.doc_id+'_'+self.annotation_id, 
-                        self.name,
-                        self.start,
-                        self.end])
-        elif (self.label == 'Element' or 
-              self.label == 'Mineral' or 
-              self.label == 'Material' or 
-              self.label == 'Feature'):
-            dbutils.insert_into_table(
-                cursor=cursor,
-                table='components',
-                columns=['component_id', 'component_name', 'component_label',
-                         'span_start', 'span_end'],
-                values=[self.doc_id+'_'+self.annotation_id, 
-                        self.name, 
-                        self.label,
-                        self.start,
-                        self.end])
-        elif self.label == 'Contains_event':
-            # Loop over all targets
-            for t in self.targets:
-                # Loop over all constituents
-                for v in self.cont:
-                    # Extract the excerpt 
-                    cursor.execute("SELECT content " +
-                                   "FROM documents " +
-                                   "WHERE doc_id='%s';" \
-                                       % (self.doc_id))
-                    content = cursor.fetchone()
-                    if content == None:
-                        print 'Warning: document %s not found, skipping.' % \
-                            self.doc_id
-                        break
+        # Basic annotations (type T in .ann files) are inserted into
+        # the anchors table, which tracks all text mentions (and spans).
+        if self.type == 'anchor':
+            canonical = ''
 
-                    content = content[0]
-                    cursor.execute("SELECT span_start, span_end " +
-                                   "FROM targets " +
-                                   "WHERE target_id='%s';" \
-                                       % (self.doc_id+'_'+self.anchor))
-                    (anchor_start,anchor_end) = cursor.fetchone()
-                    sent_start = max(content[:anchor_start].rfind('.'),0)+1
-                    sent_end   = anchor_end + \
-                        min(content[anchor_end:].find('.'),len(content))+1
-                    excerpt = content[sent_start:sent_end]
-
-                    # Insert into table
+            # Targets and components:
+            # Look up canonical entry, or add if needed.
+            if self.label in lookups.keys():
+                # Canonical name in all lower case
+                canonical = self.name.lower()
+                (tabname, colname) = lookups[self.label]
+                cursor.execute("SELECT %s FROM %s " % (colname, tabname) +
+                               "WHERE %s ILIKE '%s';" % (colname, self.name))
+                name = cursor.fetchone()
+                if name == None:
                     dbutils.insert_into_table(
                         cursor=cursor,
-                        table='contains',
-                        columns=['event_id',  'doc_id', 
-                                 'target_id', 'component_id', 
-                                 'magnitude', 'confidence',   
-                                 'annotator', 'excerpt'],
-                        values=[self.doc_id+'_'+self.annotation_id, 
-                                self.doc_id,
-                                self.doc_id+'_'+t,
-                                self.doc_id+'_'+v,
-                                'unknown',
-                                'neutral',
-                                self.username,
-                                excerpt])
-        elif (self.label == 'Amount' or
-              self.label == 'Associated' or
-              self.label == 'BelongsTo' or
-              self.label == 'Confidence' or
-              self.label == 'DoesNotContain_event' or
-              self.label == 'DoesNotShow' or
-              self.label == 'Formation' or 
-              self.label == 'IsSituatedIn' or
-              self.label == 'Locality' or
-              self.label == 'Material' or
-              self.label == 'Member' or
-              self.label == 'Position' or 
-              self.label == 'Process' or
-              self.label == 'Region' or
-              self.label == 'Shows' or
-              self.label == 'Site' or
-              self.label == 'StratRel_event' or
-              self.label == 'Unit'):
+                        table=tabname,
+                        columns=[colname],
+                        values=[canonical])
+
+            # Add this anchor.  Text other than targets and components
+            # will not have a canonical entry.  That's okay.
+            dbutils.insert_into_table(
+                cursor=cursor,
+                table='anchors',
+                columns=['anchor_id', 'label', 
+                         'canonical', 'text',
+                         'span_start', 'span_end'],
+                values=[self.doc_id+'_'+self.annotation_id, self.label,
+                        canonical,  self.name,
+                        self.start, self.end])
+
+        # Events are 
+        elif self.type == 'event':
+            if self.label == 'Contains':
+                print self.anchor
+                # Loop over all targets
+                for t in self.targets:
+                    # Loop over all constituents
+                    for v in self.cont:
+                        # Extract the excerpt 
+                        cursor.execute("SELECT content " +
+                                       "FROM documents " +
+                                       "WHERE doc_id='%s';" \
+                                           % (self.doc_id))
+                        content = cursor.fetchone()
+                        if content == None:
+                            print 'Warning: document %s not found, skipping.' % \
+                                self.doc_id
+                            break
+
+                        # Compute the likely start and end of the sentence
+                        content = content[0]
+                        cursor.execute("SELECT span_start, span_end " +
+                                       "FROM anchors " +
+                                       "WHERE anchor_id='%s';" \
+                                           % (self.doc_id+'_'+self.anchor))
+                        (anchor_start,anchor_end) = cursor.fetchone()
+                        sent_start = max(content[:anchor_start].rfind('.'),0)+1
+                        sent_end   = anchor_end + \
+                            min(content[anchor_end:].find('.'),len(content))+1
+                        excerpt = content[sent_start:sent_end]
+
+                        # Get the canonical forms of the target and component
+                        cursor.execute("SELECT canonical " +
+                                       "FROM anchors " +
+                                       "WHERE anchor_id='%s';" \
+                                           % (self.doc_id+'_'+t))
+                        canonical_t = cursor.fetchone()[0]
+                        cursor.execute("SELECT canonical " +
+                                       "FROM anchors " +
+                                       "WHERE anchor_id='%s';" \
+                                           % (self.doc_id+'_'+v))
+                        canonical_v = cursor.fetchone()[0]
+
+                        # Insert into table
+                        dbutils.insert_into_table(
+                            cursor=cursor,
+                            table='contains',
+                            columns=['event_id',  'doc_id', 'anchor_id',
+                                     'target_name', 'component_name', 
+                                     'magnitude', 'confidence',   
+                                     'annotator', 'excerpt'],
+                            values=[self.doc_id+'_'+self.annotation_id, 
+                                    self.doc_id,
+                                    self.doc_id+'_'+self.anchor,
+                                    canonical_t, canonical_v,
+                                    'unknown',
+                                    'neutral',
+                                    self.username,
+                                    excerpt])
+            elif (self.label == 'DoesNotContain' or
+                  self.label == 'StratRel'):
+                # Not yet handled
+                pass
+
+        elif self.type == 'relation':
+            # Not yet handled
+            pass
+        elif self.type == 'attribute':
             # Not yet handled
             pass
         else:
