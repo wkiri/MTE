8506d47b2d25774735638bf5583f360de119d739
Kiri Wagstaff <kiri.l.wagstaff@jpl.nasa.gov>
2016-11-15 07:10:27 -0800

    Annotate using chars instead of lines and check annotation spans as they are created.

--- src/pre_annotate.py
+++ src/pre_annotate.py
@@ -14,7 +14,7 @@ import re
 #textdir = '../text/lpsc15-C-pre-annotate-sol1159'
 #textdir = '../text/lpsc15-C-pre-annotate-sol1159-v2'
 #textdir = '../text/lpsc16-C-pre-annotate'
-textdir = '../../MTE-corpus/lpsc14-text'
+textdir = '../../MTE-corpus/lpsc16-text'
 
 # Reference files
 elementfile = '../ref/elements.txt'
@@ -33,157 +33,171 @@ for p in ['_', '\+', '-', '\.']:
 dirlist = [fn for fn in os.listdir(textdir) if
            fn.endswith('.txt')]
 
-# Process lines from input file, annotate anything that matches 'items',
+# Process text from input file, annotate anything that matches 'items',
 # and write out the annotations to outf with type 'name'.
 # Number targets starting from start_t.
-def pre_annotate(lines, items, name, outf, start_t):
+def pre_annotate(chars, items, name, outf, start_t):
     # Initialize counters
     target_ind = start_t
     span_start = 0
     span_end = 0
 
-    # Iterate through words and look for a match in the item list
-    for l in lines:
-        # Specifying ' ' explicitly means that all single spaces
-        # will cause a split.  This way we can update span_start
-        # correctly even if there are multiple spaces present.
-        words = l.split(' ')
-        # Convert UTF-8 to a Unicode string, so funky chars don't get counted
-        # as two bytes instead of one.
-        words = [w.decode('utf8') for w in words]
-        for (i,w) in enumerate(words):
-            end_of_sentence = False
-            # Remove any trailing \n etc.
-            w_strip = w.strip()
-            #print '<%s>,<%s>' % (w,w_strip)
-            # Track whether this is a potential end of sentence
-            # to avoid spurious element abbreviation annotations
-            if w_strip.endswith('.'):
-                end_of_sentence = True
-            # Remove any punctuation, except '_' and '+' (ions) and '-'
-            # and '.' (e.g., Mt. Sharp)
-            w_strip = re.sub('[%s]' % re.escape(mypunc), '', w_strip)
-            # Try the word and also the two-word and three-word phrases it's in
-            phrases = [(w, w_strip)]
-            if i < len(words)-1:
-                w_next_1 = re.sub('[%s]' % re.escape(mypunc), '', words[i+1])
-                phrases += [(' '.join([w, words[i+1]]), ' '.join([w_strip, w_next_1]))]
-            if i < len(words)-2:
-                w_next_2 = re.sub('[%s]' % re.escape(mypunc), '', words[i+2])
-                phrases += [(' '.join([w, words[i+1], words[i+2]]), 
-                             ' '.join([w_strip, w_next_1, w_next_2]))]
+    # Specifying ' ' explicitly means that all single spaces
+    # will cause a split.  This way we can update span_start
+    # correctly even if there are multiple spaces present.
+    words = chars.split(' ')  # Already Unicode
+    for (i,w) in enumerate(words):
+        # This check slows us down, but ensures we didn't mess up the indexing!
+        if w != chars[span_start:span_start+len(w)]:
+            print 'Error: <%s> and <%s> do not match.' % (w,
+                                                          chars[span_start:span_start+len(w)])
+            raw_input()
+                
+        end_of_sentence = False
 
-            for (my_word, my_word_strip) in phrases:
-                extra = 0 # Characters we stripped off but still need to count in spans
-                # If it ends with - or ., take it off
-                if (my_word_strip.endswith('-') or 
-                    my_word_strip.endswith('.')):
-                    my_word_strip = my_word_strip[:-1]
-                    extra   += 1
-                if my_word_strip in items:
-                    #print my_word, my_word_strip
-                    # For elements, skip matches that end in a period
-                    # and are short (i.e., abbreviations) because these
-                    # are more likely to be author initials.
-                    if (name == 'Element' and 
-                        end_of_sentence and 
-                        len(my_word_strip) <= 3):
-                        span_end    = span_start + len(my_word_strip)
-                    else:
-                        # This handles leading and trailing punctuation,
-                        # but not cases where there is internal punctuation
-                        try:
-                            span_start_strip = span_start + my_word.index(my_word_strip)
-                        except: # internal punctuation, so skip it
-                            print 'Skipping', my_word, my_word_strip
-                            continue
-                        span_end    = span_start_strip + len(my_word_strip)# + extra
-                        '''
-                        print '%s, %s, %s goes from %d (%d) to %d' % \
-                            (w, my_word, my_word_strip,
-                             span_start_strip,
-                             my_word.index(my_word_strip),
-                             span_end)
-                        raw_input()
-                        '''
+        # Remove any trailing \n etc.
+        w_strip = w.strip()
+        #print '<%s>,<%s>' % (w,w_strip)
+        # Track whether this is a potential end of sentence
+        # to avoid spurious element abbreviation annotations
+        if w_strip.endswith('.'):
+            end_of_sentence = True
+        # Remove any punctuation, except '_' and '+' (ions) and '-'
+        # and '.' (e.g., Mt. Sharp)
+        w_strip = re.sub('[%s]' % re.escape(mypunc), '', w_strip)
+        # Try the word and also the two-word and three-word phrases it's in
+        phrases = [(w, w_strip)]
+        if i < len(words)-1:
+            w_next_1 = re.sub('[%s]' % re.escape(mypunc), '', words[i+1])
+            phrases += [(' '.join([w, words[i+1]]), ' '.join([w_strip, w_next_1]))]
+        if i < len(words)-2:
+            w_next_2 = re.sub('[%s]' % re.escape(mypunc), '', words[i+2])
+            phrases += [(' '.join([w, words[i+1], words[i+2]]), 
+                         ' '.join([w_strip, w_next_1, w_next_2]))]
 
-                        # Format: Tx\tTarget <span_start> <span_end>\t<word>
-                        outf.write('T' + str(target_ind) + '\t' +
-                                   name + ' ' + str(span_start_strip) + 
-                                   ' ' + str(span_end) + '\t' +
-                                   my_word_strip + '\n')
-                        # Set up for the next target
-                        target_ind += 1
+        for (my_word, my_word_strip) in phrases:
+            extra = 0 # Characters we stripped off but still need to count in spans
+            # If it ends with - or ., take it off
+            if (my_word_strip.endswith('-') or 
+                my_word_strip.endswith('.')):
+                my_word_strip = my_word_strip[:-1]
+                extra   += 1
+            if my_word_strip in items:
+                #print my_word, my_word_strip
+                # For elements, skip matches that end in a period
+                # and are short (i.e., abbreviations) because these
+                # are more likely to be author initials.
+                if (name == 'Element' and 
+                    end_of_sentence and 
+                    len(my_word_strip) <= 3):
+                    span_end    = span_start + len(my_word_strip)
                 else:
-                    span_end    = span_start + len(my_word_strip) + extra
+                    # This handles leading and trailing punctuation,
+                    # but not cases where there is internal punctuation
+                    try:
+                        span_start_strip = span_start + my_word.index(my_word_strip)
+                    except: # internal punctuation, so skip it 
+                        # (fine as long as ONE of these succeeds...
+                        # otherwise span_start needs an update)
+                        print 'Skipping', my_word, my_word_strip
+                        continue
+                    span_end    = span_start_strip + len(my_word_strip)# + extra
+                    '''
+                    print '%s, %s, %s goes from %d (%d) to %d' % \
+                    (w, my_word, my_word_strip,
+                    span_start_strip,
+                    my_word.index(my_word_strip),
+                    span_end)
+                    raw_input()
+                    '''
+                    # Check that annotation and offsets match
+                    if chars[span_start_strip:span_end] != my_word_strip:
+                        print 'Error: <%s> and <%s> do not match.' % (my_word_strip,
+                                                                          chars[span_start_strip:span_end])
+                        raw_input()
+
+                    # Format: Tx\tTarget <span_start> <span_end>\t<word>
+                    outf.write('T' + str(target_ind) + '\t' +
+                               name + ' ' + str(span_start_strip) + 
+                               ' ' + str(span_end) + '\t' +
+                               my_word_strip + '\n')
+                    # Set up for the next target
+                    target_ind += 1
+            else:
+                span_end    = span_start + len(my_word_strip) + extra
                     
-                #print '<%s>, <%s>, span %d to %d' % (w, my_word, span_start, span_end)
+            #print '<%s>, <%s>, span %d to %d' % (w, my_word, span_start, span_end)
 
-            # Either way, update span_start
-            span_end   = span_start + len(w)
-            #span_start = span_end
-            span_start = span_end + 1 # Assumes followed by space 
-            # (or newline, for final word in line)
-
-        span_start -= 1 # Uncount the newline (double-count)
+        # Either way, update span_start
+        span_end   = span_start + len(w)
+        #span_start = span_end
+        span_start = span_end + 1 # Assumes followed by space 
 
     # return the latest target_ind for ongoing use
     return target_ind
 
 
-# Process lines from input file, annotate anything matching 'suffix'
+# Process text from input file, annotate anything matching 'suffix'
 # that is at least min_len in length 
 # and does not appear in nonmatches,
 # and write out the annotations to outf with type 'name'.
 # Number targets starting from start_t.
-def pre_annotate_suffix(lines, suffix, min_len, nonmatches, name, outf, start_t):
+def pre_annotate_suffix(chars, suffix, min_len, nonmatches, name, outf, start_t):
     # Initialize counters
     target_ind = start_t
     span_start = 0
     span_end = 0
 
-    # Iterate through words and look for a match 
-    for l in lines:
-        # Specifying ' ' explicitly means that all single spaces
-        # will cause a split.  This way we can update span_start
-        # correctly even if there are multiple spaces present.
-        words = l.split(' ')
-        # Convert UTF-8 to a Unicode string, so funky chars don't get counted
-        # as two bytes instead of one.
-        words = [w.decode('utf8') for w in words]
-        for w in words:
-            # Remove any trailing \n etc.
-            w_strip = w.strip()
-            # Remove any punctuation, except '_', '+' (ions) and '-'
-            w_strip = re.sub('[%s]' % re.escape(mypunc), '', w_strip)
-            if (w_strip.endswith(suffix) and 
-                len(w_strip) >= min_len and
-                w_strip not in nonmatches):
-                # This handles leading and trailing punctuation,
-                # but not cases where there is internal punctuation
-                try:
-                    span_start_strip = span_start + w.index(w_strip)
-                except:
-                    # Skip this one
-                    print 'Could not handle %s.' % w
-                    continue
-                span_end    = span_start_strip + len(w_strip) 
-                # Format: Tx\tTarget <span_start> <span_end>\t<word>
-                outf.write('T' + str(target_ind) + '\t' +
-                           name + ' ' + str(span_start_strip) + 
-                           ' ' + str(span_end) + '\t' +
-                           w_strip + '\n')
-                # Set up for the next target
-                target_ind += 1
-            else:
-                span_end    = span_start + len(w_strip) 
+    # Specifying ' ' explicitly means that all single spaces
+    # will cause a split.  This way we can update span_start
+    # correctly even if there are multiple spaces present.
+    words = chars.split(' ')  # Already Unicode
+
+    for w in words:
+        # This check slows us down, but ensures we didn't mess up the indexing!
+        if w != chars[span_start:span_start+len(w)]:
+            print 'Error: <%s> and <%s> do not match.' % (w,
+                                                          chars[span_start:span_start+len(w)])
+            raw_input()
+                
+        # Remove any trailing \n etc.
+        w_strip = w.strip()
+        # Remove any punctuation, except '_', '+' (ions) and '-'
+        w_strip = re.sub('[%s]' % re.escape(mypunc), '', w_strip)
+        if (w_strip.endswith(suffix) and 
+            len(w_strip) >= min_len and
+            w_strip not in nonmatches):
+            # This handles leading and trailing punctuation,
+            # but not cases where there is internal punctuation
+            try:
+                span_start_strip = span_start + w.index(w_strip)
+            except:
+                # Skip this one
+                print 'Could not handle %s.' % w
+                span_start += (len(w) + 1)
+                continue
+            span_end    = span_start_strip + len(w_strip) 
+
+            # Check that annotation and offsets match
+            if chars[span_start_strip:span_end] != w_strip:
+                print 'Error: <%s> and <%s> do not match.' % (w_strip,
+                                                              chars[span_start_strip:span_end])
+                raw_input()
+
+            # Format: Tx\tTarget <span_start> <span_end>\t<word>
+            outf.write('T' + str(target_ind) + '\t' +
+                       name + ' ' + str(span_start_strip) + 
+                       ' ' + str(span_end) + '\t' +
+                       w_strip + '\n')
+            # Set up for the next target
+            target_ind += 1
+        else:
+            span_end    = span_start + len(w_strip) 
                     
-            #print '<%s>, span %d to %d' % (w, span_start, span_end)
-            # Either way, update span_start
-            span_end   = span_start + len(w)
-            span_start = span_end + 1 # Assumes followed by space 
-            # (or newline, for final word in line)
-        span_start -= 1 # Uncount the newline (double-count)
+        #print '<%s>, span %d to %d' % (w, span_start, span_end)
+        # Either way, update span_start
+        span_end   = span_start + len(w)
+        span_start = span_end + 1 # Assumes followed by space 
 
     # return the latest target_ind for ongoing use
     return target_ind
@@ -262,24 +276,27 @@ print 'Read in %d MER target names.' % len(mer_targets)
 
 # Iterate through documents; output to .ann file
 for fn in dirlist:
-    #if int(fn.split('.')[0]) < 1063:
+    #if int(fn.split('.')[0]) > 1018:
     #    continue
     print fn
 
-    # Read in the input .txt document
+    # Read in all of the characters so we can track offsets
     with open(textdir + '/' + fn, 'r') as inf:
-        lines = inf.readlines()
+        # Convert UTF-8 to a Unicode string, so funky chars don't get counted
+        # as two bytes instead of one.
+        chars = inf.read().decode('utf8')
+        inf.close()
 
     annfile = textdir + '/' + fn[0:-4] + '.ann'
 
     # Create the annotations
     start_t = 1
     with io.open(annfile, 'w', encoding='utf8') as outf:
-        start_t = pre_annotate(lines, elements,        'Element', outf, start_t)
-        start_t = pre_annotate(lines, chemcam_targets, 'Target',  outf, start_t)
-#        start_t = pre_annotate(lines, mer_targets,     'Target',  outf, start_t)
-        start_t = pre_annotate_suffix(lines, 'ite', 6, nonminerals, 'Mineral', outf, start_t)
-        start_t = pre_annotate(lines, minerals,        'Mineral', outf, start_t)
+        start_t = pre_annotate(chars, elements,        'Element', outf, start_t)
+        start_t = pre_annotate(chars, chemcam_targets, 'Target',  outf, start_t)
+#        start_t = pre_annotate(chars, mer_targets,     'Target',  outf, start_t)
+        start_t = pre_annotate_suffix(chars, 'ite', 6, nonminerals, 'Mineral', outf, start_t)
+        start_t = pre_annotate(chars, minerals,        'Mineral', outf, start_t)
 #        sys.exit(0)
 
 
