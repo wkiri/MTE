53c0740b9c48896376bba803aa3779c88839ced0
Kiri Wagstaff <kiri.l.wagstaff@jpl.nasa.gov>
2021-06-08 12:30:11 -0700

    Update script to take arguments instead of hardcoded locations and make parsing consistent with journalparser.

--- src/extract_text_utf8.py
+++ src/extract_text_utf8.py
@@ -14,94 +14,95 @@ tika.TikaClientOnly = True
 from tika import parser
 from progressbar import ProgressBar, ETA, Bar, Percentage
 
-# Local files
-#pdfdir  = '../../pdfs/lpsc14-pdfs'
-#textdir = '../../MTE-corpus/lpsc14-text'
-#pdfdir  = '../../pdfs/lpsc15-pdfs'
-#textdir = '../../MTE-corpus/lpsc15-text'
-#pdfdir  = '../../pdfs/lpsc16-pdfs'
-#textdir = '../../MTE-corpus/lpsc16-text'
-#pdfdir  = '../pdfs/lpsc17-pdfs'
-#textdir = 'lpsc17-text'
-#pdfdir  = '../pdfs/lpsc18-pdfs'
-#textdir = 'lpsc18-text'
-pdfdir  = '../pdfs/lpsc19-pdfs'
-textdir = 'lpsc19-text'
+def main(pdfdir, textdir):
 
-dirlist = [fn for fn in os.listdir(pdfdir) if
-           fn.endswith('.pdf')]
+    dirlist = [fn for fn in os.listdir(pdfdir) if
+               fn.endswith('.pdf')]
 
-print 'Extracting text, using Tika, from %d files in %s.' % \
-    (len(dirlist), pdfdir)
-print '  Writing output text files to %s.' % textdir
+    print 'Extracting text, using Tika, from %d files in %s.' % \
+        (len(dirlist), pdfdir)
+    print '  Writing output text files to %s.' % textdir
 
-if not os.path.exists(textdir):
-    os.mkdir(textdir)
+    if not os.path.exists(textdir):
+        os.mkdir(textdir)
 
-widgets = ['Files (of %d): ' % len(dirlist), Percentage(), ' ', Bar('='), ' ', ETA()]
-pbar = ProgressBar(widgets=widgets, maxval=len(dirlist)).start()
+    widgets = ['Files (of %d): ' % len(dirlist), Percentage(), ' ', Bar('='), ' ', ETA()]
+    pbar = ProgressBar(widgets=widgets, maxval=len(dirlist)).start()
     
-for (i, fn) in enumerate(dirlist):
-    pbar.update(i)
-    #if int(fn.split('.')[0]) not in [1523, 1662, 1853, 2403, 2573, 1856, 2038, 2124, 2504, 2595]:
-    #    continue
-    #print fn
+    for (i, fn) in enumerate(dirlist):
+        pbar.update(i)
+        #if int(fn.split('.')[0]) != 1001:
+        #    continue
+        #print fn
+        parsed = parser.from_file(pdfdir + '/' + fn)
 
-    outfile = os.path.join(textdir, fn[0:-4] + '.txt')
-    # If the output file already exists, skip it
-    if os.path.exists(outfile):
-        continue
-
-    # Parse content with Tika
-    parsed = parser.from_file(pdfdir + '/' + fn)
-
-    try:
-        if parsed['content'] == None:
-            print 'Tika found no content in %s.' % fn
+        try:
+            if parsed['content'] == None:
+                print 'Tika found no content in %s.' % fn
+                import pdb; pdb.set_trace()
+                continue
+        except:
+            print 'Tika could not parse %s.' % fn
             continue
-    except:
-        print 'Tika could not parse %s.' % fn
-        continue
 
-    with io.open(outfile, 'w', encoding='utf8') as outf:
-        cleaned = parsed['content']
+        with io.open(textdir + '/' + fn[0:-4] + '.txt', 'w', 
+                     encoding='utf8') as outf:
+            cleaned = parsed['content']
 
-        # Translate some UTF-8 punctuation to ASCII
-        punc = { 0x2018:0x27, 0x2019:0x27, # single quote
-                 0x201C:0x22, 0x201D:0x22, # double quote
-                 0x2010:0x2d, 0x2011:0x2d, 0x2012:0x2d, 0x2013:0x2d, # hyphens
-                 0xFF0C:0x2c, # comma
-                 0x00A0:0x20, # space
-                 0x2219:0x2e, 0x2022:0x2e, # bullets
+            # Translate some UTF-8 punctuation to ASCII
+            punc = { 0x2018:0x27, 0x2019:0x27, # single quote
+                     0x201C:0x22, 0x201D:0x22, # double quote
+                     0x2010:0x2d, 0x2011:0x2d, 0x2012:0x2d, 0x2013:0x2d, # hyphens
+                     0xF0B0:0xb0, # degree
+                     0xFF0C:0x2c, # comma
+                     0x00A0:0x20, # space
+                     0x2219:0x2e, 0x2022:0x2e, # bullets
                  }
-#                 0x005E:0x5e, 0x02C6:0x5e, 0x0302:0x5e, 0x2038:0x5e, # carets
-#                 0x00B0:0x6f, 0x02DA:0x6f, # degree
-#                 0x00B9:0x31, 0x00B2:0x32, 0x00B3:0x33, # exponents
-        cleaned = cleaned.translate(punc)
+            # 0x005E:0x5e, 0x02C6:0x5e, 0x0302:0x5e, 0x2038:0x5e, # carets
+            # 0x00B0:0x6f, 0x02DA:0x6f, # degree
+            # 0x00B9:0x31, 0x00B2:0x32, 0x00B3:0x33, # exponents
+            cleaned = cleaned.translate(punc)
 
-        # Replace newlines that separate words with a space (unless hyphen)
-        cleaned = re.sub(r'([^\s-])[\r|\n]+([^\s])','\\1 \\2', cleaned)
+            # Replace newlines that separate words with a space (unless hyphen)
+            cleaned = re.sub(r'([^\s-])[\r|\n]+([^\s])','\\1 \\2', cleaned)
 
-        # Remove hyphenation at the end of lines 
-        # (this is sometimes bad, as with "Fe-\nrich")
-        cleaned = cleaned.replace('-\n','\n')
+            # Remove hyphenation at the end of lines 
+            # (this is sometimes bad, as with "Fe-\nrich")
+            cleaned = cleaned.replace('-\n','\n')
 
-        # Remove all newlines
-        cleaned = re.sub(r'[\r|\n]+','', cleaned)
+            # Remove all newlines
+            cleaned = re.sub(r'[\r|\n]+','', cleaned)
 
-        # Remove xxxx.PDF
-        cleaned = re.sub(r'([0-9][0-9][0-9][0-9].PDF)', '', cleaned,
-                         flags=re.IGNORECASE)
-        # And "Lunar and Planetary Science Conference (201x)"
-        cleaned = re.sub(r'([0-9][0-9].. Lunar and Planetary Science Conference \(201[0-9]\))', 
-                         '', cleaned,
-                         flags=re.IGNORECASE)
+            # Remove xxxx.PDF
+            cleaned = re.sub(r'([0-9][0-9][0-9][0-9].PDF)', '', cleaned,
+                             flags=re.IGNORECASE)
+            # And "xx(th|st) Lunar and Planetary Science Conference ((19|20)xx)"
+            # with optional parentheses, optional LPI contrib
+            cleaned = re.sub(r'([0-9][0-9].. Lunar and Planetary Science Conference \(?(19|20)[0-9][0-9]\)?)( \(LPI Contrib. No. [0-9][0-9][0-9][0-9]\))? ?',
+                             '', cleaned,
+                             flags=re.IGNORECASE)
+            # And "Lunar and Planetary Science XXXIII (2002)"
+            # with Roman numeral and optional year
+            cleaned = re.sub(r'(Lunar and Planetary Science [CDILVXM]+( \((19|20)[0-9][0-9]\))?) ?', 
+                             '', cleaned,
+                             flags=re.IGNORECASE)
 
-        # Remove mailto: links
-        cleaned = re.sub(r'mailto:[^\s]+','', cleaned)
+            # Remove mailto: links
+            cleaned = re.sub(r'mailto:[^\s]+','', cleaned)
 
-        outf.write(cleaned)
-        outf.close()
+            outf.write(cleaned)
+            outf.close()
+
+
+if __name__ == '__main__':
+    import argparse
+    ps = argparse.ArgumentParser(argument_default=argparse.SUPPRESS, 
+                                 description='Extract (UTF-8) text from PDFs')
+
+    ps.add_argument('pdfdir',  help='Location of PDF files to extract')
+    ps.add_argument('textdir', help='Output directory for text files')
+    args = ps.parse_args()
+    main(**vars(args))
 
 
 # Copyright 2015, by the California Institute of Technology. ALL
