6724aa418d6fdf3b89199d6772519443ffe63af5
Steven Y Lu <you.lu@jpl.nasa.gov>
2021-09-24 17:44:46 -0700

    use brat ann files to identify Contains and HasProperty relations #19

--- src/jsre_labeling_corenlp_and_brat.py
+++ src/jsre_labeling_corenlp_and_brat.py
@@ -3,43 +3,48 @@
 # Read in a text file, run CoreNLP to get features (pos, lemma, NER),
 # and convert it to JSRE's data ("examples") format.
 #
-# Authors: Kiri Wagstaff and Karanjeet Singh
-# Created on: March 13, 2017
-# Copyright notice at bottom of file.
+# Authors: Kiri Wagstaff, Karanjeet Singh, Steven Lu
+# Created on: September 24, 2021
+# Copyright notice at bottom of file
 
-import sys, os, io
+import os
+import io
+import sys
+import json
 import urllib
+import itertools
+from pycorenlp import StanfordCoreNLP
+
 # The following two lines make CoreNLP happy
 reload(sys)
 sys.setdefaultencoding('UTF8')
-import argparse
-from pycorenlp import StanfordCoreNLP
-from pysolr import Solr
 
 
-def parse(txt_file, ann_file, accept_entities):
+def parse(txt_file, ann_file, accept_entity_types):
     with open(txt_file) as text_file, open(ann_file) as ann_file:
         texts = text_file.read().decode('utf8')
         text_file.close()
         anns = map(lambda x: x.strip().split('\t'), ann_file)
-        anns = filter(lambda x: len(x) > 2, anns)
+
         # FIXME: ignoring the annotations which are complex
+        anns = filter(lambda x: len(x) > 2, anns)
 
+        # FIXME: some annotations' spread have been split into many, separated
+        # FIXME: by ; ignoring them
         anns = filter(lambda x: ';' not in x[1], anns)
-
-        # FIXME: some annotations' spread have been split into many, separated by ; ignoring them
 
         def __parse_ann(ann):
             spec = ann[1].split()
             entity_type = spec[0]
             ann_id = ann[0]
             markers = list(map(lambda x: int(x), spec[1:]))
-            # t = ' '.join([texts[begin:end] for begin,end in zip(markers[::2], markers[1::2])])
             t = texts[markers[0]:markers[1]]
             if not t == ann[2]:
-                print("Error: Annotation mis-match, file=%s, ann=%s, text=%s" % (txt_file, str(ann), t))
+                print("Error: Annotation mis-match, file=%s, ann=%s, text=%s" %
+                      (txt_file, str(ann), t))
                 return None
-            return (entity_type, markers, t, ann_id)
+
+            return entity_type, markers, t, ann_id
 
         anns = map(__parse_ann, anns)  # format
         anns = filter(lambda x: x, anns)  # skip None
@@ -47,7 +52,7 @@ def parse(txt_file, ann_file, accept_entities):
         # building a tree index for easy accessing
         tree = {}
         for entity_type, pos, name, ann_id in anns:
-            if entity_type in accept_entities:
+            if entity_type in accept_entity_types:
                 begin, end = pos[0], pos[1]
                 if begin not in tree:
                     tree[begin] = {}
@@ -71,24 +76,21 @@ def include_brat_ann(entities, brat_tree):
         if e_begin in brat_tree:
             node = brat_tree[e_begin]
             if len(node) > 1:
-                #print("WARN: multiple starts at ", e_begin, node)
                 if e_end in node:
-                    #e['ann_id'] = node[e_end][0]  # picking one
                     node = {e_end: node[e_end]}  # picking one
-                    #print("Chose:", node)
             ann_end, labels = node.items()[0]
             if not len(labels) == 1:
-                print("WARN: Duplicate ids for token: %s, id:%s. Using the first one!" % (e['word'], str(labels)))
+                print("WARN: Duplicate ids for token: %s, id:%s. Using the "
+                      "first one!" % (e['word'], str(labels)))
             label = labels[0]
             if e_end == ann_end:  # annotation ends where token ends
                 continue_ann = None
             elif e_end < ann_end and label != 'O':
-                #print("Continue for the next %d chars" % (ann_end - e_end))
                 continue_ann = label
                 continue_ann_end = ann_end
                 continue_ann_begin = e_begin
-        elif continue_ann is not None and e_end <= continue_ann_end and e_begin > continue_ann_begin:
-            #print("Continuing the annotation %s, %d:%d %d]" % (continue_ann, e_begin, e_end, continue_ann_end))
+        elif continue_ann is not None and e_end <= continue_ann_end and \
+                e_begin > continue_ann_begin:
             label = continue_ann  # previous label is this label
             if continue_ann_end == e_end:  # continuation ends here
                 print("End")
@@ -99,9 +101,9 @@ def include_brat_ann(entities, brat_tree):
             e['ann_id'] = label
 
 
-def generate_example_id(fnbase, index, ex_id):
+def generate_example_id(fn_base, index, ex_id):
     # Create a unique identifier
-    return '%s_%s_%s' % (fnbase, str(index), str(ex_id))
+    return '%s_%s_%s' % (fn_base, str(index), str(ex_id))
 
 
 def generate_example(id, label, sentence, target_index, active_index):
@@ -125,138 +127,186 @@ def generate_example(id, label, sentence, target_index, active_index):
                                              entity_label)
     # Output the example
     example = '%s\t%s\t%s\n' % (label, id, body)
-    print example
+
     return example
 
 
-def build_jsre_examples(rel, venue, in_path, out_path, solr_url, corenlp_url):
-    """
-    Build jSRE examples from CoreNLP NER and Brat Annotations
-    :param rel: relation to extract
-    :param venue: example: lpsc15
-    :param in_path: path to the input directory
-    :param out_path: path to the output directory
-    :param solr_url: URL of Solr Server
-    :param corenlp_url: URL of Stanford CoreNLP Server
-    :return:
-    """
-    # Configuration
-    corenlp_server = StanfordCoreNLP(corenlp_url)
-    solr_server = Solr(solr_url)
-    props = {'annotators': 'tokenize,ssplit,lemma,pos,ner',
-             #'ner.model': 'ner_model_train_63r15v2_685k14-no-ref_384k15-no-ref.ser.gz',
-             #'ner.model': 'ner_model_train_62r15_685k14_384k15.ser.gz',
-             # won't work because model inside isn't 'old'
-             #'ner.model': 'ner_model_train_62r15v3_emt_gazette_old.ser.gz',
-             'ner.model': 'ner_model_train_62r15v3_emt_gazette.ser.gz',
-             #'ner.model': 'ner_62r15v3_emt_gazette.ser.gz',
-             'outputFormat': 'json'}
+def parse_relation(ann_file):
+    with open(ann_file, 'r') as f:
+        lines = f.readlines()
 
-    if not os.path.exists(out_path):
-        print 'Creating output directory %s' % out_path
-        os.mkdir(out_path)
+        # In brat annotation, the entity type (e.g., Target, Element, Mineral)
+        # is indicated with letter T and the relation type (e.g., Contains,
+        # HasProperty) is indicated with letter R
+        relations = [l.strip().split() for l in lines if l.startswith('R')]
+        entities = [l.strip().split() for l in lines if l.startswith('T')]
 
-    # Select *.txt files
-    in_files = [fn for fn in os.listdir(in_path) if fn.endswith('.txt')]
+    entity_dict = dict()
+    for entity in entities:
+        entity_dict[entity[0]] = entity[4]
+
+    ann_relation_dict = dict()
+    for rel in relations:
+        ann_relation_dict[rel[0]] = {
+            'relation_type': rel[1],
+            'arg1': rel[2].split(':')[1],
+            'arg2': rel[3].split(':')[1]
+        }
+
+    return ann_relation_dict
+
+
+def has_relation(target, component, relation, ann_relation_dict):
+    brat_relation_mapping = {
+        'contains': 'Contains',
+        'has_property': 'HasProperty'
+    }
+    relation_type = brat_relation_mapping[relation.lower()]
+
+    if len(ann_relation_dict.keys()) == 0:
+        return False
+
+    for v in ann_relation_dict.values():
+        if v['relation_type'] == relation_type and \
+                v['arg1'].lower() == target.lower() and \
+                v['arg2'].lower() == component.lower():
+            return True
+
+    return False
+
+
+def build_jsre_examples(relation, in_dir, out_dir, ner_model, corenlp_url):
+    if not os.path.exists(in_dir) or not os.path.isdir(in_dir):
+        print '[ERROR] in_dir does not exist or is not a directory.'
+        sys.exit(1)
+
+    if not os.path.exists(ner_model) or not os.path.isfile(ner_model):
+        print '[ERROR] ner_model does not exist or is not a file.'
+        sys.exit(1)
+
+    # Initialize Stanford CoreNLP server
+    corenlp = StanfordCoreNLP(corenlp_url)
+    props = {
+        'annotators': 'tokenize,ssplit,lemma,pos,ner',
+        'outputFormat': 'json',
+        'ner.useSUTime': False,
+        'ner.applyNumericClassifiers': False,
+        'timeout': '60000',
+        'ner.applyFineGrained': False,
+        'ner.model': ner_model
+    }
+
+    # Create output directory
+    if not os.path.exists(out_dir):
+        print 'Creating output directory: %s' % os.path.abspath(out_dir)
+        os.mkdir(out_dir)
+
+    # Figure out entity types
+    accept_entity_types = ['Target']
+    if relation.lower() == 'contains':
+        accept_entity_types.append('Element')
+        accept_entity_types.append('Mineral')
+    else:
+        accept_entity_types.append('Property')
+    print 'Accepted entity types: %s' % json.dumps(accept_entity_types)
+
+    # Select .txt files
+    in_files = [fn for fn in os.listdir(in_dir) if fn.endswith('.txt')]
     in_files.sort()
-    print 'Processing %d documents. ' % len(in_files)
+    print 'Processing %d documents.' % len(in_files)
 
     for fn in in_files:
-        fnbase = fn[:fn.find('.txt')]
-        in_fn = '%s.txt' % fnbase
-        ann_fn = '%s.ann' % fnbase
-        out_fn = '%s-%s.examples' % (fnbase, rel.lower())
+        fn_base = fn[: fn.find('.txt')]
+        txt_fn = '%s.txt' % fn_base
+        ann_fn = '%s.ann' % fn_base
+        out_fn = '%s-%s.examples' % (fn_base, relation.lower())
+        ann_relation_dict = parse_relation(os.path.join(in_dir, ann_fn))
 
-        print 'Reading in %s and %s' % (in_fn, ann_fn)
-        #for line in convert(corenlp_server, os.path.join(in_path, in_fn), os.path.join(in_path, ann_fn)):
-        #    print line
-        accept_entities = {'Target', rel}
-        text, tree = parse(os.path.join(in_path, in_fn), os.path.join(in_path, ann_fn), accept_entities)
+        print 'Reading in %s and %s' % (txt_fn, ann_fn)
 
+        text, tree = parse(os.path.join(in_dir, txt_fn),
+                           os.path.join(in_dir, ann_fn), accept_entity_types)
+
+        # Reason: some tools trim/strip off the white spaces which will mismatch
+        # the character offsets
         if text[0].isspace():
             text = '.' + text[1:]
-            # Reason: some tools trim/strip off the white spaces
-            #  which will mismatch the character offsets
 
         # Running CoreNLP on Document
         # Quote (with percent-encoding) reserved characters in URL for CoreNLP
         text = urllib.quote(text)
-        doc = corenlp_server.annotate(text, properties=props)
+        doc = corenlp.annotate(text, properties=props)
 
-        with io.open(os.path.join(out_path, out_fn), 'w', 
-                     encoding='utf8') as outf:
-            # Map Raymond's .ann (brat) annotations into the CoreNLP-parsed
-            # sentence/token structure.
-            ex_id = 0
-            for s in doc['sentences']:
-                # For each pair of target+(element|mineral) entities,
-                # are they in a contains relationship?
-                # label:
-                # 0 - negative
-                # 1 - entity_1 contains entity_2
-                # 2 - entity_2 contains entity_1
-                # Get the relevant entities (Target, Element, Mineral)
-                targets = [t for t in s['tokens'] if t['ner'] == 'Target']
-                active = [t for t in s['tokens'] if t['ner'] == rel]
+        out_file = io.open(os.path.join(out_dir, out_fn), 'w', encoding='utf8')
 
-                include_brat_ann(targets, tree)
-                include_brat_ann(active, tree)
+        # Map brat annotations into CoreNLP sentence/token structures
+        example_id = 0
+        for s in doc['sentences']:
+            targets = [t for t in s['tokens'] if t['ner'] == 'Target']
+            active = [t for t in s['tokens']
+                      if t['ner'] in accept_entity_types[1:]]
 
-                for i in range(0, len(targets)):
-                    for j in range(0, len(active)):
-                        if 'ann_id' not in targets[i]:
-                            print "No annotation exists for Target:", targets[i]['word']
-                            id = generate_example_id(fnbase, s['index'], ex_id)
-                            example = generate_example(id, 0, s, 
-                                                       targets[i]['index'], 
-                                                       active[j]['index'])
-                            outf.write(example)
-                            ex_id += 1
-                            continue
-                        if 'ann_id' not in active[j]:
-                            print 'No annotation exists for %s: %s' % (rel, active[j]['word'])
-                            id = generate_example_id(fnbase, s['index'], ex_id)
-                            example = generate_example(id, 0, s,
-                                                       targets[i]['index'], 
-                                                       active[j]['index'])
-                            outf.write(example)
-                            ex_id += 1
-                            continue
-                        label = 0
-                        # This gives a positive label for an exact match
-                        # on the Target and active string spans.
-                        print 'Querying %s contains %s.' % \
-                            (targets[i]['word'], active[j]['word'])
-                        qry_str = 'type:contains AND p_id:' + venue + '-' + fnbase + \
-                                  ' AND targets_ss:' + targets[i]['ann_id'] + \
-                                  ' AND cont_ss:' + active[j]['ann_id']
-                        print qry_str, "\n"
-                        resp = solr_server.search(qry_str, None, fl='id')
-                        if resp and resp.hits > 0:
-                            label = 1
+            include_brat_ann(targets, tree)
+            include_brat_ann(active, tree)
 
-                        # Create a unique identifier
-                        id = generate_example_id(fnbase, s['index'], ex_id)
-                        ex_id += 1
-                        example = generate_example(id, label, s, 
-                                                   targets[i]['index'], 
-                                                   active[j]['index'])
-                        outf.write(example)
-                        print
+            entity_pairs = itertools.product(targets, active)
+            for target, component in entity_pairs:
+                if 'ann_id' not in target:
+                    print "No annotation exists for Target:", target['word']
+                    id = generate_example_id(fn_base, s['index'], example_id)
+                    example = generate_example(id, 0, s,
+                                               target['index'],
+                                               component['index'])
+                    out_file.write(example)
+                    example_id += 1
+                    continue
+
+                if 'ann_id' not in component:
+                    print 'No annotation exists for %s: %s' % (relation,
+                                                               component)
+                    id = generate_example_id(fn_base, s['index'], example_id)
+                    example = generate_example(id, 0, s,
+                                               target['index'],
+                                               component['index'])
+                    out_file.write(example)
+                    example_id += 1
+                    continue
+
+                label = 0
+                if has_relation(target, component, relation, ann_relation_dict):
+                    label = 1
+
+                id = generate_example_id(fn_base, s['index'], example_id)
+                example_id += 1
+                example = generate_example(id, label, s,
+                                           target['index'],
+                                           component['index'])
+                out_file.write(example)
+
+        out_file.close()
 
 
 if __name__ == '__main__':
+    import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--relation', help='Mineral or Element', required=True)
-    parser.add_argument('-v', '--venue', help='Example: lpsc15', required=True)
-    parser.add_argument('-i', '--input', help='Directory path to documents containing text and annotations', required=True)
-    parser.add_argument('-o', '--output', help='Directory path where jSRE examples to be placed', required=True)
-    parser.add_argument('-s', '--solr_url', help='URL of Solr Server; Default: http://localhost:8983/solr/docs',
-                        default='http://localhost:8983/solr/docs')
-    parser.add_argument('-n', '--corenlp_url', help='URL of Stanford CoreNlp server; Default: http://localhost:9000',
-                        default='http://localhost:9000')
+    parser.add_argument('-r', '--relation', required=True,
+                        choices=['contains', 'has_property'],
+                        help='The valid options are contains or has_property')
+    parser.add_argument('-i', '--in_dir', required=True,
+                        help='Directory path to documents containing text '
+                             '(.txt) and annotations (.ann)')
+    parser.add_argument('-o', '--out_dir', required=True,
+                        help='Directory path to the output jSRE examples')
+    parser.add_argument('-n', '--ner_model',
+                        help='Path to a Named Entity Recognition (NER) model')
+    parser.add_argument('-c', '--corenlp_url', default='http://localhost:9000',
+                        help='URL of Stanford CoreNLP server. The default is '
+                             'http://localhost:9000')
+
     args = parser.parse_args()
-    build_jsre_examples(args.relation, args.venue, args.input, args.output, args.solr_url, args.corenlp_url)
+
+    build_jsre_examples(**vars(args))
 
 
 # Copyright 2017, by the California Institute of Technology. ALL
@@ -270,4 +320,3 @@ if __name__ == '__main__':
 # responsibility to obtain export licenses, or other export authority
 # as may be required before exporting such information to foreign
 # countries or providing access to foreign persons.
-
